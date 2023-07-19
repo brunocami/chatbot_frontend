c756ebc4a4a933c289fb641edb2a0ca9eb591775
brunocami <bruno.cami@hotmail.com>
2023-07-19 19:57:31 -0300

    update navbar

--- src/components/chatForm.js
+++ src/components/chatForm.js
@@ -4,32 +4,28 @@ function ChatForm() {
     return (
         <form>
             <div className="form-group">
-                <label htmlFor="exampleFormControlInput1">Email address</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                <label htmlFor="exampleFormControlInput1">Sales Person Name</label>
+                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Ted Lasso" />
             </div>
             <div className="form-group">
-                <label htmlFor="exampleFormControlSelect1">Example select</label>
-                <select className="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
+                <label htmlFor="exampleFormControlInput1">Sales Person Role</label>
+                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Representante de Ventas" />
             </div>
             <div className="form-group">
-                <label htmlFor="exampleFormControlSelect2">Example multiple select</label>
-                <select multiple className="form-control" id="exampleFormControlSelect2">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
+                <label htmlFor="exampleFormControlInput1">Company Name</label>
+                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Sleep Heaven" />
             </div>
             <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <label htmlFor="exampleFormControlTextarea1">Company Business</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Sleep Haven es una compañía de colchones premium que brinda...'></textarea>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleFormControlTextarea1">Company Values</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Nuestra misión en Sleep Haven es ayudar a las personas a lograr una mejor noche...'></textarea>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleFormControlTextarea1">Conversation Purpose</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Responder a las necesidades del cliente y buscar venderle uno de los colchones del catalogo'></textarea>
             </div>
         </form>
     )
